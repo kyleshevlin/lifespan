12de5cde2734329b5dfdc641aae10ca437093bdd
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2018-09-08 09:40:44 -0700

    tidy a few things

--- app/js/reducers/index.js
+++ app/js/reducers/index.js
@@ -1,4 +1,4 @@
-import { WEEKS_IN_LIFE } from '../constants'
+import { MILLISECONDS_IN_A_WEEK, WEEKS_IN_LIFE } from '../constants'
 import * as types from '../constants/actionTypes'
 
 const initialWeeks = []
@@ -16,21 +16,18 @@ const initialWeeksState = {
   weeksLived: 0
 }
 
-const initialState = Object.assign({}, initialWeeksState, {
+const initialState = {
+  ...initialWeeksState,
   birthdate: '',
   todayTime: new Date().getTime()
-})
+}
 
 const handleBirthdateInput = (state, action) => {
   const birthdate = action.value
   const pattern = /^(0[1-9]|1[0-2])[/](0[1-9]|[12]\d|3[01])[/](19|20)\d{2}$/
-  let weeksState
-
-  if (pattern.test(birthdate)) {
-    weeksState = updateWeeks(state, action)
-  } else {
-    weeksState = initialWeeksState
-  }
+  const weeksState = pattern.test(birthdate)
+    ? updateWeeks(state, action)
+    : initialWeeksState
 
   return { ...state, ...weeksState, birthdate }
 }
@@ -38,19 +35,19 @@ const handleBirthdateInput = (state, action) => {
 const updateWeeks = (state, action) => {
   const birthdateTime = new Date(action.value).getTime()
   const weeksLived = Math.floor(
-    (state.todayTime - birthdateTime) / (1000 * 3600 * 24 * 7)
+    (state.todayTime - birthdateTime) / MILLISECONDS_IN_A_WEEK
   )
 
   if (weeksLived < 0) {
     return initialWeeksState
-  } else {
-    const newWeeks = state.weeks.map((week, index) => {
-      const hasBeenLived = index < weeksLived
-      return { ...week, hasBeenLived }
-    })
-
-    return { weeks: newWeeks, weeksLived }
   }
+
+  const newWeeks = state.weeks.map((week, index) => ({
+    ...week,
+    hasBeenLived: index < weeksLived
+  }))
+
+  return { weeks: newWeeks, weeksLived }
 }
 
 const rootReducer = (state = initialState, action) => {
