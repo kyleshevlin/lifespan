912029a534607af6df33eac29a8c528a508b4b4c
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2020-07-10 09:42:26 -0700

    major dependency overhaul

--- app/js/Store.js
+++ app/js/Store.js
@@ -1,7 +1,6 @@
 import { createStore } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
 import reducer from './reducers'
 
-const store = createStore(reducer, composeWithDevTools())
+const store = createStore(reducer)
 
 export default store
